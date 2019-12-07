07207194534fb245bd8440dfd5c8e63b8f42bc66
Shark <mauricion978@gmail.com>
2019-12-06 21:43:00 -0300

    Nova Atualização do aplicativo

--- teorema-de-pitagoras.py
+++ teorema-de-pitagoras.py
@@ -8,7 +8,7 @@ import math
 janela = Tk()
 janela.geometry("700x285")
 janela.title("Teorema de Pitágoras")
-image2 = Image.open("dicas-de-fórmula-de-pitágoras.jpg")
+image2 = Image.open("formula-pitagoras.jpg")
 image1 = ImageTk.PhotoImage(image2)
 background_label = Label(janela, image=image1)
 background_label.image1=image1
@@ -26,20 +26,20 @@ def Pitagoras(hip):
 
 #-------------------------------= Labels =-------------------------------------
 ed1 = Entry(janela)
-ed1.pack()
+ed1.place(x=75, y=250)
 
 ed2 = Entry(janela)
-ed2.pack()
+ed2.place(x=200, y=250)
 
 lb = Label(janela, text="")
-lb.pack()
+lb.place(x=330, y=250)
 #---------------------------------------------------------------------------
 
 
 #------------------------------= Botôes =-------------------------------------
 bt = Button(janela, text="Pitagoras")
 bt["command"] = partial(Pitagoras, bt)
-bt.pack()
+bt.place(x=12, y=245)
 
 #-----------------------------------------------------------------------------
 
